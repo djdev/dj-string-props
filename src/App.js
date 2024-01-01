133ca13747cc2061dbba25752236a19ff1195eb0
djdev <deepjyoti_rajkhowa@yahoo.com>
2024-01-01 14:17:38 -0500

    Updated component body and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './style.css';
 
+function Badge({ name, handle, img }) {
+  return (
+    <div className="badge">
+      <img alt={name} src={img} />
+      <div>
+        <h4>{name}</h4>
+        <p>@{handle}</p>
+      </div>
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Badge
+      name="DJ Rajkhowa"
+      handle="dj_rajkhowa"
+      img="https://avatars.githubusercontent.com/u/2480220?s=400&u=7737ef7a910c97a14a25f1ebac474d99fa467a02&v=4"
+    />
   );
 }
